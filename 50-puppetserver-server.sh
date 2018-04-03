2aa339d4cdcca7b83fa4fb973e46a3016629b455
Jesse Weisner <jesse_weisner@bcit.ca>
2018-04-03 13:18:51 -0700

    symlink etc

--- 50-puppetserver-server.sh
+++ 50-puppetserver-server.sh
@@ -1,1 +1,6 @@
 [ -d /opt/puppetlabs/server/data ] || tar zxf /opt/puppetlabs/server.tar.gz -C /opt/puppetlabs
+[ -f /opt/puppetlabs/server.tar.gz ] && rm -f /opt/puppetlabs/server.tar.gz
+[ -d /opt/puppetlabs/server/data/puppetserver/.puppetlabs ] && rm -rf /opt/puppetlabs/server/data/puppetserver/.puppetlabs
+mkdir -p /opt/puppetlabs/server/data/puppetserver/.puppetlabs/etc
+ln -sf /etc/puppetlabs/puppet /opt/puppetlabs/server/data/puppetserver/.puppetlabs/etc/puppet
+
