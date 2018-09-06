f793233a9544fc4b1367c5fe64436887f6e52e0a
Jesse Weisner <jesse_weisner@bcit.ca>
2018-09-05 18:28:45 -0700

    Fix sed not replacing puppet user

--- 10-resolve-userid.sh
+++ 10-resolve-userid.sh
@@ -1,4 +1,4 @@
 if [ "$RUNUSER" = "puppet" ];then
-    sed -i "s/puppet:x:999:998/puppet:x:${UID}:0/" /etc/passwd
-    sed -i "s/puppet:x:998:/puppet:x:998:puppet/" /etc/group
+    sed -i "s/^puppet:.*/puppet:x:${UID}:0/" /etc/passwd
+    sed -i "s/^puppet:.*/puppet:x:998:puppet/" /etc/group
 fi
